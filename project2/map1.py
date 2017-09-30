188e897167db1ff9db5ee4896e2deabb28f0c00d
Daniel Melo <danfmelo@gmail.com>
2017-09-29 23:14:57 -0300

    adding mapbox, collors and layers

--- project2/map1.py
+++ project2/map1.py
@@ -7,14 +7,32 @@ lon = list(data["LON"])
 lat = list(data["LAT"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+	if elevation < 1000:
+		return 'green'
+	elif 1000 <= elevation < 3000:
+		return 'orange'
+	else:
+		return 'red' 
+
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=8, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el), icon=folium.Icon(color='green')))
-	map.add_child(fg)
+	fgv.add_child(folium.Marker(location=[lt, ln], popup=str(el), icon=folium.Icon(color=color_producer(el))))
 
+fgp = folium.FeatureGroup(name="Population")
+
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 1000000
+else 'orange' if 1000000 <= x['properties']['POP2005'] < 2000000 else 'red'}))
+
+
+map.add_child(fgp)
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
 
